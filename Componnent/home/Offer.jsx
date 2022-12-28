a09082a3dbbed7c446facc539568f2403d7074e7
Md Emon Hossen <mdemong87@gmail.com>
2022-12-28 13:06:50 +0600

    'update'

--- Componnent/home/Offer.jsx
+++ Componnent/home/Offer.jsx
@@ -1,21 +1,25 @@
 import img from "../../public/default-img.jpg";
+import styles from "../../styles/home/Offer.module.css";
 
 export default function Offer() {
 
     const style = {
 
-        background: `linear-gradient(#00000065,#00000065),url('${img.src}')`,
+        background: `linear-gradient(#00000075,#00000075),url('${img.src}')`,
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
         backgroundSize: "Cover",
-        height: "100%"
+        height: "100%",
+        padding: "10px 15px"
     }
 
 
 
     return (
         <div style={style}>
-
+            <h4 className={styles.head}>Save 15% with Late Escape Deals</h4>
+            <p className={styles.dis}>Enjoy the freedom of a monthly stay on Booking.com</p>
+            <button className={styles.btn}>Explore Deal</button>
         </div>
     )
 }
