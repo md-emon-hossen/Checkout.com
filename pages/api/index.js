87c8972ed6127b281bf4a876d180c2d462989a17
Md Emon Hossen <mdemong87@gmail.com>
2022-12-28 11:25:47 +0600

    update;

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,20 +1,5 @@
-import connectDB from "../../mongodb/connect";
-import Student from "../../mongodb/model";
-
 
 export default async function handler(req, res) {
 
-  await connectDB();
-   await Student.create({name:"emon hossen",age:"20"});
-
-   Student.find((err,data)=>{
-    if(!err){
-      res.status(200).json(data)
-    }else{
-      res.status(500).json({error:"server error"});
-    }
-
-   });
-
-
+  res.status(200).json({ name: "checkout" });
 }
