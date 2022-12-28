c51138b7b9e83fb949aad2b109ca04a65e45431d
Md Emon Hossen <mdemong87@gmail.com>
2022-12-28 13:25:21 +0600

    update

---
+++ Componnent/home/Explore.jsx
@@ -0,0 +1,24 @@
+import img from "../../public/default-img.jpg";
+import styles from "../../styles/home/Explore.module.css";
+
+
+export default function Explore() {
+    const style = {
+
+        background: `linear-gradient(#00000075,#00000075),url('${img.src}')`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundSize: "Cover",
+        height: "100%",
+        padding: "10px 15px"
+    }
+
+    return (
+        <div style={style}>
+            <div className={styles.titleWRp}>
+                <h4>Dhaka</h4>
+                <p>255 Propertise</p>
+            </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Arrow from "../Componnent/Arrow";
 import Container from "../Componnent/Container";
+import Explore from "../Componnent/home/Explore";
 import Offer from "../Componnent/home/Offer";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <Container>
+
+      {/* offers */}
       <div className={styles.offerWrp}>
         <h3>Offers</h3>
         <p>Promotions, deals, and special offers for you</p>
@@ -15,6 +18,22 @@ export default function Home() {
         </div>
       </div>
       <Arrow />
+
+      {/* explore */}
+      <div className={styles.explore}>
+        <h3>Explore Bangladesh</h3>
+        <p>These popular destinations have a lot to offer</p>
+        <div className={styles.ExloreItemWrp}>
+          <Explore />
+          <Explore />
+          <Explore />
+          <Explore />
+        </div>
+      </div>
+      <Arrow />
+
+
+
     </Container>
   )
 }
